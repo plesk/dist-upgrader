e40050ecd0935d6aea4dc23dd74efa3331801879
Kirill Pushkarev <kirill.pushkarev@webpros.com>
2024-07-09 13:48:57 +0700

    Remove unused defs.py

--- defs.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2023-2024. WebPros International GmbH. All rights reserved.
-# vim:ft=python:
-
-with allow_unsafe_import():
-    import subprocess
-
-def get_git_revision():
-    return subprocess.check_call(['git', 'rev-parse', 'HEAD'])
