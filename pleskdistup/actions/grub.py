38010b306c4e4648b6fdaf252e1f0148028ac5bf
Umit Kablan <umit.kablan@webpros.com>
2024-09-23 10:38:07 +0300

    FIX Check GRUB install_device's for inaccessible files (device)
    PAUX-6367: Distupgrade tool does not handle issue with GRUB misconfiguration

---
+++ pleskdistup/actions/grub.py
@@ -0,0 +1,30 @@
+# Copyright 2023-2024. WebPros International GmbH. All rights reserved.
+import os.path
+
+from pleskdistup.common import action, util
+
+DEBCONF_CMD = "/usr/bin/debconf-show"
+
+class AssertGrubInstallDeviceExists(action.CheckAction):
+    def __init__(self) -> None:
+        self.name = "check GRUB installation device exists"
+        self.description = """Grub's install-device is not found: {}
+This could fail conversion - please fix GRUB configuration:
+    dpkg --configure grub-pc
+"""
+
+    def _do_check(self) -> bool:
+        if not os.path.exists(DEBCONF_CMD):
+            return True
+
+        param = "grub-pc"
+        debconf_out = util.logged_check_call([DEBCONF_CMD, param])
+        prefix = "{}/install_devices:".format(param)
+        for line in debconf_out.split("\n"):
+            idx = line.find(prefix)
+            if idx != -1:
+                devpath = line[idx+len(prefix):].strip()
+                if not os.path.exists(devpath):
+                    self.description = self.description.format(devpath)
+                    return False
+        return True
